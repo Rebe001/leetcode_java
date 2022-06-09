75999135089ccb2382311f0a9d313d848395f2f7
rebeccali-sage <rebecca112211@gmail.com>
2022-06-09 14:42:24 +0200

    add solution to challenge 374 and 278, refactor challenge 35

--- 35. search input position/solution.java
+++ 35. search input position/solution.java
@@ -1,11 +1,13 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        //binary search 
-       int low = 0, high = nums.length; 
-        while (low < high){
-            //prevent overflow 
+        if (nums == null|| nums.length == 0) return 0; 
+        //iterative binary search 
+       int low = 0, high = nums.length -1; 
+        while (low <= high){
+            //to prevent overflow 
             int mid = (high - low )/2 + low ;
-            if (nums[mid] >= target) high = mid;
+            if (nums[mid] == target) return mid;  
+            else if (nums[mid] > target) high = mid - 1;
             else low = mid + 1;
         }
         return low;
